517ed5618f18d1dd6703f5bb9bc9d6ba675a92de
Tara-Oliver <tara_m_oliver@yahoo.com>
2023-09-15 01:08:30 -0400

    completed problem_set2

--- problem_set2.ts
+++ problem_set2.ts
@@ -1,3 +1,23 @@
+/*
+
+Intro:
+
+    All 2 users liked the idea of the community. We should go
+    forward and introduce some order. We are in Germany after all.
+    Let's add a couple of admins.
+
+    Initially, we only had users in the in-memory database. After
+    introducing Admins, we need to fix the types so that
+    everything works well together.
+
+Exercise:
+
+    Type "Person" is missing, please define it and use
+    it in persons array and logPerson function in order to fix
+    all the TS errors.
+
+*/
+
 interface User {
 	name: string;
 	age: number;
